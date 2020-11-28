a05edee5f0f208c785cd7915d0d630ed220911ee
Juan  Manuel <masync.j@gmail.com>
2020-11-28 01:26:08 -0500

    Add heroku profile, main.py and data.py  On branch main

---
+++ data.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+products = [
+    
+    {"name":"julian soto", 
+     "email":"jdsoto@gmail.com"},  
+    {"name":"julian correa",
+     "email":"jcorrea@gmail.com"},
+    {"name":"Oliver charry", 
+     "email":"ofcharry@gmail.com"},  
+    {"name":"Mateo Garcia",
+     "email":"mgarcia@gmail.com"},
+    {"name":"Emilio Peña", 
+     "email":"ejpena@gmail.com"},  
+    {"name":"Juan Patiño",
+     "email":"jmpatino@gmail.com"}  
+    ]
---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import FastAPI
+
+from data import products
+
+app = FastAPI()
+
+@app.get('/getnames')
+async def getnames():
+    arr_data= []
+    for idx in range(0, len(products)):
+        item_data = products[idx].get("name")
+        arr_data.append(item_data)
+    return  arr_data
+
+
+@app.get('/')
+async def home_page(): 
+    return {"data": products}
+
+
+@app.post('/home/{item_id}')
+async def post_item(item_id:str ):
+    arr_data= []
+    for idx in range(0, len(products)):
+        item_data = products[idx].get(item_id)
+        arr_data.append(item_data)
+    return  arr_data
+
